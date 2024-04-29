f626b330dd271b53de8d51423c6f98c68294e57f
Théophile Delmas <contact@theophile.world>
2024-04-30 00:23:34 +0200

    Add Hash & Pass

--- api/models/user.go
+++ api/models/user.go
@@ -8,7 +8,8 @@ type User struct {
     gorm.Model
     Username  string    `json:"username" gorm:"unique;not null"`
     Email     string    `json:"email" gorm:"unique;not null"`
-    Password  string    `json:"-" gorm:"not null"` // Password is not returned in JSON responses
+    Password  string    `json:"password" gorm:"not null"` // Password is not returned in JSON responses
+    PasswordHash  string    `json:"-" gorm:"not null"` // Password is not returned in JSON responses
     Bounties  []Bounty  `json:"bounties"` // User can have multiple bounties
 }
 
