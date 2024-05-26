6e7d62c8e057a65dc248f5c99afc27663394a9b9
Théophile Delmas <contact@theophile.world>
2024-05-26 16:01:25 +0200

    Update colors

--- front/pages/about.vue
+++ front/pages/about.vue
@@ -1,22 +1,35 @@
 <template>
-  <section class="bg-gray-800 container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold text-center mb-4 text-primary">Open Bounties</h1>
-    <p class="text-lg text-gray-200 text-center mb-8">
-      Open Bounties is a revolutionary platform where open source enthusiasts can sponsor development by putting bounties on GitHub issues. Enhance the progress of your favorite projects by financially supporting specific development tasks.
+  <section class="bg-secondary-dark container mx-auto px-4 my-6 py-6 rounded-lg">
+    <h1 class="text-4xl font-bold text-center mb-6 text-primary-light">Join Bount.ing</h1>
+    <p class="text-lg text-info-light text-center mb-10">
+      Revolutionizing open source development. Contribute, learn, and earn with every code commit while advancing sustainability.
     </p>
     <div class="flex justify-center">
-      <img src="https://via.placeholder.com/350x200.png?text=Open+Bounties" alt="Open Bounties Image" class="shadow-custom rounded-lg mb-6">
+      <img src="https://via.placeholder.com/400x225.png?text=Bount.ing" alt="Bount.ing Image" class="shadow-lg rounded-lg mb-8">
     </div>
-    <h2 class="text-2xl font-semibold mb-3 text-secondary">How It Works</h2>
-    <ul class="list-disc pl-5 mb-6">
-      <li>Connect your GitHub account to get started.</li>
-      <li>Choose issues you want to sponsor in your favorite repositories.</li>
-      <li>Set a bounty and watch the community tackle it with enthusiasm.</li>
+    <h2 class="text-2xl font-semibold mb-4 text-primary-light">How It Works</h2>
+    <ul class="list-disc pl-5 mb-8 text-info-light">
+      <li>Connect your GitHub account to track and sponsor issues.</li>
+      <li>Pick challenges in your favorite projects and set bounties.</li>
+      <li>Fix issues to earn bounties and sharpen your skills.</li>
     </ul>
-    <h2 class="text-2xl font-semibold mb-3 text-secondary">Join Us</h2>
-    <p class="mb-6">Become a part of the Open Bounties community today and help accelerate the development of open source projects. Your contribution makes a difference!</p>
+    <h2 class="text-2xl font-semibold mb-4 text-primary-light">How to Claim a Bounty</h2>
+    <p class="text-lg text-info-light mb-4">
+      Link your pull request to the related GitHub issue. After issue closure, we verify contributions and credit your account automatically. Unclaimed bounties transform into future opportunities or refunds.
+    </p>
+    <h2 class="text-2xl font-semibold mb-4 text-primary-light">Our Mission</h2>
+    <p class="text-lg text-info-light mb-8">
+      Founded on May 4, 2024, in Barcelona, Bount.ing is committed to fostering independent work, embracing open-source values, promoting learning while earning, and advancing sustainability in technology.
+    </p>
+    <h2 class="text-2xl font-semibold mb-4 text-primary-light">Join Our Community</h2>
+    <p class="mb-8 text-info-light">Connect with fellow developers and open-source enthusiasts. Share, learn, and grow together in our vibrant Discord community.</p>
     <div class="flex justify-center">
-        <button class="bg-primary hover:bg-dark text-white font-bold py-2 px-4 rounded-lg">
+      <a href="https://discord.gg/cj5Wjfa5pR" target="_blank" class="bg-primary hover:bg-success text-primary-light font-bold py-2 px-4 rounded-lg">
+        Join Our Discord
+      </a>
+    </div>
+    <div class="flex justify-center mt-8">
+      <button class="bg-primary hover:bg-success text-info-dark font-bold py-2 px-4 rounded-lg">
         Sign Up Now
       </button>
     </div>
@@ -30,5 +43,5 @@ export default {
 </script>
 
 <style scoped>
-
+/* Add any specific styles here */
 </style>
