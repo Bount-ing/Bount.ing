1ebb61e08fc10fd49514371dd713b7c987ee251f
Théophile Delmas <contact@theophile.world>
2024-05-26 16:19:29 +0200

    Add privacy and tos

--- front/components/Footer.vue
+++ front/components/Footer.vue
@@ -1,8 +1,17 @@
 <template>
-  <footer class="bg-dark text-soft py-16">
-    <div class="container mx-auto px-4 sm:px-6 lg:px-8 text-center space-y-2">
-      <p>© 2023 OpenBounties. All rights reserved.</p>
-      <p>Follow us on <a href="#" class="underline text-info">Social Media</a></p>
+  <footer class="bg-secondary-dark text-primary-light py-16">
+    <div class="container mx-auto px-4 sm:px-6 lg:px-8 text-center space-y-4">
+      <p>© 2024 Bount.ing. All rights reserved.</p>
+      <p>Follow us on
+        <a href="https://twitter.com/yourTwitter" class="underline text-info">Twitter</a>,
+        <a href="https://facebook.com/yourFacebook" class="underline text-info">Facebook</a>,
+        <a href="https://instagram.com/yourInstagram" class="underline text-info">Instagram</a>
+      </p>
+      <p>
+        <NuxtLink to="/terms" class="underline text-info">Terms of Service</NuxtLink> |
+        <NuxtLink to="/privacy" class="underline text-info">Privacy Policy</NuxtLink>
+      </p>
+      <p>Contact us: <a href="mailto:support@bount.ing" class="underline text-info">support@bount.ing.com</a></p>
     </div>
   </footer>
 </template>
