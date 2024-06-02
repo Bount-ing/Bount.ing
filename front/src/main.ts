c51ffa8d0b8534ef8a65b9cd19ce468272f4732d
Théophile Delmas <contact@theophile.world>
2024-06-02 14:04:15 +0200

    rm vuetify

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,25 +1,19 @@
 import './assets/main.css'
+import './assets/tailwind.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify';
-import '@mdi/font/css/materialdesignicons.css';
-import 'vuetify/styles';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 import  createGtag  from 'vue-gtag-next';
 import type GtagPluginOptions from 'vue-gtag-next'
 
 
 import App from './App.vue'
 import router from './router'
-import './assets/tailwind.css'
 
 const app = createApp(App)
-const vuetify = createVuetify()
 
 app.use(createPinia())
-app.use(vuetify)
 app.use(router)
 
 
