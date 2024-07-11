007acb578943691282e165d9f25b88f565f28c68
sebamiro <seba.mi.21@gmail.com>
2024-07-11 13:39:59 +0200

    add: StripeInvoiceID to bounty model

--- api/models/bounty.go
+++ api/models/bounty.go
@@ -19,18 +19,19 @@ const (
 
 type Bounty struct {
 	gorm.Model
-	Amount         float64   `json:"amount" gorm:"not null"`
-	BountyType     string    `json:"bounty_type" gorm:"default:'flat'"`
-	Currency       string    `json:"currency" gorm:"default:'EUR'"`
-	IssueGithubID  int       `json:"issue_github_id" gorm:"not null"`
-	IssueGithubURL string    `json:"issue_github_url" gorm:"not null"`
-	IssueImageURL  string    `json:"issue_image_url" gorm:"not null"`
-	StartAt        time.Time `json:"start_at" gorm:"not null"`
-	EndAt          time.Time `json:"end_at" gorm:"not null"`
-	OwnerID        uint      `json:"owner_id" gorm:"not null"`
-	IssueID        uint      `json:"issue_id" gorm:"not null"`
-	Claims         []Claim   `json:"claims" gorm:"many2many:bounty_claims;"`
-	Status         string    `json:"status" gorm:"default:'open'"`
+	Amount          float64   `json:"amount" gorm:"not null"`
+	BountyType      string    `json:"bounty_type" gorm:"default:'flat'"`
+	Currency        string    `json:"currency" gorm:"default:'EUR'"`
+	IssueGithubID   int       `json:"issue_github_id" gorm:"not null"`
+	IssueGithubURL  string    `json:"issue_github_url" gorm:"not null"`
+	IssueImageURL   string    `json:"issue_image_url" gorm:"not null"`
+	StartAt         time.Time `json:"start_at" gorm:"not null"`
+	EndAt           time.Time `json:"end_at" gorm:"not null"`
+	OwnerID         uint      `json:"owner_id" gorm:"not null"`
+	IssueID         uint      `json:"issue_id" gorm:"not null"`
+	StripeInvoiceID string    `json:"stripe_invoice_id"`
+	Claims          []Claim   `json:"claims" gorm:"many2many:bounty_claims;"`
+	Status          string    `json:"status" gorm:"default:'open'"`
 }
 
 func ValidateBountyType(bt string) error {
