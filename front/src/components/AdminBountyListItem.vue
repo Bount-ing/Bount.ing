05d8d0132bbec79f12d6d2b9a6cac5f02542524d
sebamiro <seba.mi.21@gmail.com>
2024-07-21 18:49:20 +0200

    add: AdminBountyListItem component

--- front/src/components/AdminBountyListItem.vue
+++ front/src/components/AdminBountyListItem.vue
@@ -4,6 +4,9 @@
       <p class="text-gray-300">{{ bounty.description }}</p>
       <p class="text-gray-400">{{ $t('Amount') }}: {{ bounty.amount }}</p>
       <p class="text-gray-400">{{ $t('Status') }}: {{ bounty.status }}</p>
+      <button @click="$emit('confirm-bounty', bounty.ID)" class="mt-2 p-2 bg-green-600 text-white rounded">
+        {{ $t('Confirm') }}
+        </button>
       <button @click="$emit('cancel-bounty', bounty.ID)" class="mt-2 p-2 bg-red-600 text-white rounded">
         {{ $t('Cancel') }}
         </button>
