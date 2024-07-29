ccd4bbd4c82f17da93e88b439c5dbe07daf7e0f5
sebamiro <seba.mi.21@gmail.com>
2024-07-29 18:52:25 +0200

    fix: userID type

--- api/middleware/admin.go
+++ api/middleware/admin.go
@@ -13,15 +13,15 @@ func Admin(s *services.UserService) gin.HandlerFunc {
 		if !ok {
 			c.AbortWithStatus(http.StatusUnauthorized)
 		}
-		id, ok := userID.(uint)
+		id, ok := userID.(float64)
 		if !ok {
 			c.AbortWithStatus(http.StatusUnauthorized)
 		}
-		u, err := s.FindUserById(id)
+		u, err := s.FindUserById(uint(id))
 		if err != nil {
 			c.AbortWithStatus(http.StatusUnauthorized)
 		}
-		if u.Username != "thdelmas" {
+		if u.Username != "thdelmas" && u.Username != "sebamiro" {
 			c.AbortWithStatus(http.StatusUnauthorized)
 		}
 		c.Next()
