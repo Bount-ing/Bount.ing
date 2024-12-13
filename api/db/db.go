b3d55060efbfd351a9922eec627660fc7bde6bdc
Théophile Delmas <contact@theophile.world>
2024-12-13 17:55:28 +0100

    Db creation if doesn't exists

--- api/db/db.go
+++ api/db/db.go
@@ -1,19 +1,49 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"gorm.io/gorm"
-
+	"github.com/jackc/pgx/v4" // Use pgx to directly check and create database
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func init() {
+	// Database connection string
 	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+
+	// Connect to PostgreSQL (using "postgres" as dbname to connect to default db)
+	var err error
+	DB, err = gorm.Open(
+		postgres.New(postgres.Config{
+			DSN:                  dsn,
+			PreferSimpleProtocol: true,
+		}),
+		&gorm.Config{TranslateError: true},
+	)
+
+	if err != nil {
+		log.Fatalf("failed to establish database connection: %v", err)
+	}
+
+	// Check if the "bounting" database exists
+	if err := createDatabaseIfNotExists(); err != nil {
+		log.Fatalf("failed to create or connect to database: %v", err)
+	}
+
+	// Switch to the "bounting" database for further operations
+	dsn = fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_USER"),
@@ -22,15 +52,48 @@ func init() {
 		os.Getenv("POSTGRES_PORT"),
 	)
 
-	var err error
-
 	DB, err = gorm.Open(
 		postgres.New(postgres.Config{
 			DSN:                  dsn,
 			PreferSimpleProtocol: true,
 		}),
-		&gorm.Config{TranslateError: true})
+		&gorm.Config{TranslateError: true},
+	)
+
 	if err != nil {
-		log.Fatalf("failed to establish database connection: %v", err)
+		log.Fatalf("failed to switch to the 'bounting' database: %v", err)
 	}
+}
+
+// createDatabaseIfNotExists checks if the database exists and creates it if not
+func createDatabaseIfNotExists() error {
+	// Connect using "postgres" database to check if the "bounting" DB exists
+	conn, err := pgx.Connect(context.Background(), fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_PORT"),
+	))
+	if err != nil {
+		return fmt.Errorf("unable to connect to the database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Check if "bounting" database exists
+	var exists bool
+	err = conn.QueryRow(context.Background(), "SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname = $1)", os.Getenv("POSTGRES_DB")).Scan(&exists)
+	if err != nil {
+		return fmt.Errorf("failed to check if database exists: %v", err)
+	}
+
+	// If it doesn't exist, create it
+	if !exists {
+		_, err := conn.Exec(context.Background(), fmt.Sprintf("CREATE DATABASE %s", os.Getenv("POSTGRES_DB")))
+		if err != nil {
+			return fmt.Errorf("failed to create database: %v", err)
+		}
+	}
+
+	return nil
 }
--- api/go.mod
+++ api/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/go-mail/mail v2.3.1+incompatible
 	github.com/golang-jwt/jwt/v4 v4.5.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
-	github.com/stripe/stripe-go/v74 v74.30.0
+	github.com/jackc/pgx/v4 v4.18.3
 	gorm.io/driver/postgres v1.5.10
 	gorm.io/gorm v1.25.12
 )
@@ -24,8 +24,13 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
