2b9277064a90091308bbf7cedf9b7b24e4eac002
Théophile Delmas <contact@theophile.world>
2025-01-18 13:05:16 +0100

    ExtUser & mail design

---
+++ api/models/claim_check.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"github.com/bount-ing/bount.ing/api/db"
+	"gorm.io/gorm"
+)
+
+// PRVerification represents the verification details of a pull request
+type ClaimCheck struct {
+	gorm.Model
+	Found            bool   `json:"found"`
+	Linked           bool   `json:"linked"`
+	Accepted         bool   `json:"accepted"`
+	Closed           bool   `json:"closed"`
+	AuthorUsername   string `json:"authorUsername"`
+	AuthorExternalID uint   `json:"authorExternalId"`
+	RepoOwner        string `json:"repoOwner"`
+	RepoName         string `json:"repoName"`
+	PRNumber         string `json:"prNumber"`
+
+	// Check Attribution
+	CheckerID   uint   `json:"checker_id"`   // ID of the user who made the check (0 for system checks)
+	CheckerType string `json:"checker_type"` // "SYSTEM", "OWNER", or "CLAIMER"
+
+	// Relationship to parent Claim
+}
+
+func init() {
+	db.DB.AutoMigrate(&ClaimCheck{})
+}
---
+++ api/models/external_identity.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"github.com/bount-ing/bount.ing/api/db"
+	"gorm.io/gorm"
+)
+
+type ExternalIdentity struct {
+	gorm.Model
+	ID       uint
+	HostID   uint
+	Username string
+	Email    string
+
+	// Relationships
+	UserID uint `gorm:"not null"` // Foreign key
+	User   User `gorm:"references:ID"`
+	Host   Host `gorm:"foreignKey:HostID"`
+}
+
+func init() {
+	db.DB.AutoMigrate(&ExternalIdentity{})
+}
