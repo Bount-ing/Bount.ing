429fb8532aae1679a8099576f600a8026df90b14
Théophile Delmas <contact@theophile.world>
2025-04-06 10:14:27 +0200

    Rmv contactform

--- wui/src/views/ContactView.vue
+++ wui/src/views/ContactView.vue
@@ -5,30 +5,7 @@
       We're here to help! If you have any questions, suggestions, or need assistance with Open Bounties, please don't
       hesitate to reach out.
     </p>
-    <div class="max-w-md mx-auto p-8 border border-primary rounded-lg shadow-custom bg-secondary">
-      <form @submit.prevent="submitForm">
-        <div class="mb-6">
-          <label for="name" class="block mb-2 text-sm font-medium">Your Name:</label>
-          <input v-model="formData.name" type="text" id="name" name="name" required
-            class="bg-secondary-light border border-primary text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5" />
-        </div>
-        <div class="mb-6">
-          <label for="email" class="block mb-2 text-sm font-medium">Your Email:</label>
-          <input v-model="formData.email" type="email" id="email" name="email" required
-            class="bg-secondary-light border border-primary text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5" />
-        </div>
-        <div class="mb-16">
-          <label for="message" class="block mb-2 text-sm font-medium">Message:</label>
-          <textarea v-model="formData.message" id="message" name="message" rows="4" required
-            class="bg-secondary-light border border-primary text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"></textarea>
-        </div>
-        <button type="submit"
-          class="w-full border bg-primary border-primary hover:bg-primary-dark focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white">
-          Send Message
-        </button>
-      </form>
-    </div>
-
+   
     <!-- Contact Links Section -->
     <div class="mt-12 text-center">
       <h2 class="text-2xl font-semibold mb-4">Other Ways to Reach Us</h2>
